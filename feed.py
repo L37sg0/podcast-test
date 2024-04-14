83680ee6fac95c8163787dae43e017dbe55eca76
Petar Ivanov <petar_1v@abv.bg>
2024-04-14 09:20:02 +0000

    Save last work - not finished

--- feed.py
+++ feed.py
@@ -10,10 +10,29 @@ with open('feed.yaml', 'r') as file:
         'xmlns:content':'https://purl.org/rss/1.0/modules/content'
     })
 
-channel_element = xml_tree.SubElement(rss_element, 'channel')
+    channel_element = xml_tree.SubElement(rss_element, 'channel')
 
-xml_tree.SubElement(channel_element, 'title').text = yaml_data['title']
+    link_prefix = yaml_data['link']
+    for k in yaml_data:
+        
+        match k:
+            case 'author':
+                element_title = 'itunes:author'
+                break
+            case 'image':
+                element_title = 'itunes:image'
+                break
+            case default:
+                element_title = k
+
+        if k == 'item':
+            for x in yaml_data[k]:
+                item_element = xml_tree.SubElement(channel_element, 'item')
+                item_data = x
+                for y in item_data:
+                    xml_tree.SubElement(item_element, y).text = item_data[y]
+        else:
+            xml_tree.SubElement(channel_element, element_title).text = yaml_data[k]
 
 output_tree = xml_tree.ElementTree(rss_element)
 output_tree.write('podcast', encoding='UTF-8', xml_declaration=True)
-
